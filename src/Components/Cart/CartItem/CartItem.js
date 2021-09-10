4d7620c409eee59ff3b3460c59116c573cc2b0d8
João Costa <costav1402@gmail.com>
2021-09-10 14:41:50 -0300

    image right in cart

--- src/Components/Cart/CartItem/CartItem.js
+++ src/Components/Cart/CartItem/CartItem.js
@@ -7,7 +7,7 @@ export default function CartItem({item, formatCurrency}) {
     },[])
     return (
         <li className="cartItem" key={item.id}>
-            <img className="cartItem__image" src={item.image} alt={`Foto de ${item.name}`}/>
+            <img className="cartItem__image" src={item.images[0]} alt={`Foto de ${item.name}`}/>
             <div className="cartItem__info">
                 <h6 className="cartItem__info-title">{item.name} </h6>
                 <span className="cartItem__info-qty">{`${Math.ceil(item.qty)} unidades`}</span>

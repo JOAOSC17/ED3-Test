ef1e2d2678e501274f5906befabcf24251d7b493
João Costa <costav1402@gmail.com>
2021-09-13 18:47:56 -0300

    fixing error of decorationMar

--- src/Components/StayConnect/StayConnect.js
+++ src/Components/StayConnect/StayConnect.js
@@ -5,7 +5,7 @@ export default function StayConnect() {
        <section className="product-decoration">
          <h5 className="product-decoration__title">#STAYCONNECTED</h5>
          <div className="product-decoration__images">
-         <img src={'https://ed3.netlify.app/assets/assets/decorationMar.jpg'} draggable="false" alt="Pessoa Nadando no Mar"/>
+         <img src={'https://ed3.netlify.app/assets/decorationMar.jpg'} draggable="false" alt="Pessoa Nadando no Mar"/>
          <img src={'https://ed3.netlify.app/assets/decorationMotocicle.jpg'} draggable="false" alt="Pessoa Andando de Motocicleta"/>
          <img src={'https://ed3.netlify.app/assets/decorationFuckRacism.jpg'} draggable="false" alt="Fuck Racism"/>
          </div>
